daa13df2840eaa3ff4b2d7b2c3c0656e960f635a
Aron Cutler <acutler84@gmail.com>
2017-09-01 14:13:04 -0700

    Wrote skeleton for store, root, reducer, root component, and index.jade file

---
+++ Staged/frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Root extends React.Component {
+  render () {
+    return (<div>
+              REACT IS WORKING
+            </div>)
+  }
+}
+
+export default Root;
