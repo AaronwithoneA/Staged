29e06e490af1d42bc9b0a6ccc8b25d7d0fa1fa1b
Aron Cutler <acutler84@gmail.com>
2017-09-04 03:12:14 -0700

    Add index route to react router

--- Staged/frontend/components/root.jsx
+++ Staged/frontend/components/root.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app.jsx';
 
 const Root = ({store}) => {
   return (
     <Provider store={store}>
       <Router history={hashHistory}>
+        <Route path="/" component={ App }>
 
+        </Route>
+        </Route>
       </Router>
     </Provider>
   )
