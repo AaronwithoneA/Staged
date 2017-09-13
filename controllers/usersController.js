0089582796286066960fd7025488e13931f1a423
Matt McGregor <mttmcgrgr@gmail.com>
2017-09-12 17:22:59 -0700

    setup basic user routing, controller and model and add middleware

---
+++ controllers/usersController.js
@@ -0,0 +1,36 @@
+
+var mongoose = require('mongoose'),
+  User = mongoose.model('User');
+
+exports.list_all_users = function(req, res) {
+  User.find({}, function(err, result) {
+    if(err)
+      res.send(err);
+    res.json(user);
+  });
+};
+
+exports.create_user = function(req, res) {
+  var new_user = new User(req.body);
+  new_user.save(function(err, result){
+    if(err)
+      res.send(err);
+    res.json(result);
+  });
+};
+
+exports.show_user = function(req, res) {
+  User.findById(req.params.userId, function(err, result){
+    if(err)
+      res.send(err):
+    res.json(result);
+  })
+}
+
+exports.delete_user = function(req, res) {
+  User.remove({_id: req.params.userId}, function(err, result){
+    if(err)
+      res.send(err);
+    res.json(result);
+  })
+}
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  User = require('./models/userModel'),
+  bodyParser = require('body-parser');
 
 app.listen(port);
 
-console.log('listening on port: ' + 3000);
+console.log('listening on port: ' + port);
